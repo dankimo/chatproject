82726ca3c8ee05f9b5a7a5f4eb1cbea4a03df7bb
Dan Kimmorley <dan@thinksmartit.com.au>
2022-01-03 11:13:06 +1000

    implemented redux functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
+import Chat from './chat/chat';
+import Process from './process/process';
+import Home from './home/home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
 import React from 'react';
 import io from 'socket.io-client';
 
-function App() {
-    const socket = io('/');
+const socket = io.connect('/');
+
+function Appmain(props) {
     return (
-        <div>
-            <p>Test</p>
-        </div>
+        <React.Fragment>
+            <div className='right'>
+                <Chat 
+                    username={props.match.params.username}
+                    roomname={props.match.params.roomname}
+                    socket={socket}
+                />
+            </div>
+            <div className='left'>
+                <Process />
+            </div>
+        </React.Fragment>
     )
 }
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import rootReducers from './store/reducer/index';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+const store = crateStore(rootReducers);
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
